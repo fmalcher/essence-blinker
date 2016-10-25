da6b8d8d1d77c88f34f8781fedc11ec5a79b5f96
Ferdinand Malcher <mail@ferdinand-malcher.de>
2016-10-25 13:18:30 +0200

    blinkDelay in global const

--- main.c
+++ main.c
@@ -8,6 +8,9 @@ uint8_t outR = PB2;
 uint8_t inL = PB3;
 uint8_t inR = PB4;
 
+int const blinkDelay = 50; // blinker speed
+
+/**************************/
 
 uint8_t leftOn = 0;
 uint8_t rightOn = 0;
@@ -22,11 +25,8 @@ int main(void) {
     DDRB &= ~(1 << inR);
     PORTB |= (1 << inR); // pull-up
 
-
-
     // middle on
     PORTB |= (1 << outM);
-
 
     while(1){
         if(!(PINB & (1 << inL))) {
@@ -43,16 +43,17 @@ int main(void) {
             blinkerOff();
         }
     }
-    
+
     return 0;
 }
 
+// toggles pin and thus, processes a half blink cycle
 void blink(uint8_t pin) {
-    int delay = 50;
     PORTB ^= (1 << pin);
-    _delay_ms(delay);
+    _delay_ms(blinkDelay);
 }
 
+// both blinkers off
 void blinkerOff() {
     PORTB &= ~(1 << outL);
     PORTB &= ~(1 << outR);
