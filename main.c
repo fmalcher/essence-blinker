8abfd0dbc9e005c7b3898b2cf756f2d4afa4eeb6
Ferdinand Malcher <mail@ferdinand-malcher.de>
2016-10-30 15:27:55 +0100

    added self check

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ int main(void) {
     DDRA |= (1 << PA0);
     DDRA |= (1 << PA1);
     DDRA |= (1 << PA2);
+    DDRA |= (1 << PA3);
     DDRA |= (1 << PA4);
     DDRA |= (1 << PA5);
     DDRA |= (1 << PA6);
@@ -27,8 +28,47 @@ int main(void) {
     DDRB &= ~(1 << PB6);
     PORTB |= (1 << PB6); // pull-up
 
+
     onM();
     offBlinker();
+
+    // self check
+    PORTA |= (1 << PA4);
+    _delay_ms(100);
+    PORTA &= ~(1 << PA4);
+    PORTA |= (1 << PA6);
+    _delay_ms(100);
+    PORTA &= ~(1 << PA6);
+    PORTA |= (1 << PA7);
+    _delay_ms(100);
+    PORTA &= ~(1 << PA7);
+
+    // beep
+    int i;
+    for(i = 0; i < 20; i++) {
+        PORTA |= (1 << PA3);
+        _delay_ms(5);
+        PORTA &= ~(1 << PA3);
+        _delay_ms(5);
+     }
+    
+     for(i = 0; i < 60; i++) {
+        PORTA |= (1 << PA3);
+        _delay_ms(2);
+        PORTA &= ~(1 << PA3);
+        _delay_ms(2);
+     }
+
+     for(i = 0; i < 110; i++) {
+        PORTA |= (1 << PA3);
+        _delay_ms(1);
+        PORTA &= ~(1 << PA3);
+        _delay_ms(1);
+     }
+
+     
+
+
 
     while(1){
         if(!(PINB & (1 << PB6))) {
@@ -40,7 +80,7 @@ int main(void) {
             _delay_ms(blinkDelay);
             continue;
 
-        } else if(!(PINB & (1 << PB5))) {
+        } else if(!(PINB & (1 << PB4))) {
             if(!rightOn) offBlinker();
             leftOn = 0;
             rightOn = 1;
@@ -48,7 +88,7 @@ int main(void) {
             toggleR();
             _delay_ms(blinkDelay);
 
-        } else if(!(PINB & (1 << PB4))) {
+        } else if(!(PINB & (1 << PB5))) {
             if(!leftOn) offBlinker();
             leftOn = 1;
             rightOn = 0;
